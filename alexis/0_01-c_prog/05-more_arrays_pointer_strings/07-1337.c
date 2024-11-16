221d81d5d10f88ba0cd4adee5c773f8040b0603f
Mayor_6930p <josepholumayor@gmail.com>
2024-11-16 23:30:45 +0100

    Fixed up things, need to keep moving

--- alexis/0_01-c_prog/05-more_arrays_pointer_strings/07-1337.c
+++ alexis/0_01-c_prog/05-more_arrays_pointer_strings/07-1337.c
@@ -1,18 +1,24 @@
+/**
+ * Enc_1337 - function to encode a string in leet
+ * @inarr: The array to encode
+ * Return: A pointer to the encoded array
+ **/
+
 #include "Mayor.h"
 
 char *Enc_1337(char *inarr)
 {
-	char val[6] = "a,e,o,l,t";
-	char code[6] = "4,3,0,7,1";
+	char val[6] = "aeolt";
+	char code[6] = "43017";
 	int i, ii;
 
 	for (i = 0; inarr[i] != '\0'; i++)
 	{
-		for (ii = 0; val[ii] <= 't'; ii++)
-			{
-				if (inarr[i] == val[ii] || inarr[i] == (val[ii] - 32))
-					inarr[i] = code[ii];
-			}
+		for (ii = 0; val[ii] != '\0'; ii++)
+		{
+			if (inarr[i] == val[ii] || inarr[i] == (val[ii] - 32))
+				inarr[i] = code[ii];
+		}
 	}
 
 	return (inarr);
