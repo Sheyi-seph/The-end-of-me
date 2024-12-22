218bac1563683add12beed127f14a64ba4fc490f
Mayor_6930p <josepholumayor@gmail.com>
2024-12-22 16:40:52 +0100

    fk d neps, but major progress achievedgaa!

--- alexis/0_01-c_prog/08-args.c/04-P_add.c
+++ alexis/0_01-c_prog/08-args.c/04-P_add.c
@@ -1,11 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-
-int main(int argc, char *argv)
+int NOSYMIN(char **, int);
+int main(int argc, char *argv[])
 {
-	int sum = 0
+	int i = 1;
+	int sum = 0;
+	int symcheck;
 
-	if (argc > 1)
+
+	if (NOSYMIN(argv, argc))
 	{
+		symcheck = 1;
+	}
+	else
+	{
+		printf("Error");
+		return (1);
+	}
 
+	if(argc > 1)
+	{
+		for (; i < argc; i++)
+		{
+			sum += atoi(argv[i]);
+		}
+		printf("%d\n", sum);
+		return (0);
+	}
+	else
+	{
+		printf("0\n");
+		return (0);
+	}
+
+}
+int NOSYMIN(char **str, int cou)
+{
+	for (int i = 1; i < cou; i++)
+	{
+		for (int j = 0; str[i][j] != '\0'; j++)
+		{
+			if (str[i][j] < '0' || str[i][j] > '9')
+				return (0);
+		}
+	}
+	return (1);
 }
