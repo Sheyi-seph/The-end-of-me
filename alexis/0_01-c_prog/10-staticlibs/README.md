e8f88909a7e21ae682869c5c162bb6dce7af408f
Mayor termux <adurasheyiakinlolu@gmail.com>
2025-01-24 19:31:20 +0100

    partial work on static lib, need to refine algo

---
+++ alexis/0_01-c_prog/10-staticlibs/README.md
@@ -0,0 +1,7 @@
+-------------------------------
+working on static libraries!
+-------------------------------
+library funcs:
+
+- simple_calc.c
+performs operations such as +, -, * or /.
---
+++ alexis/0_01-c_prog/10-staticlibs/simple_calc.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int simple_calc()
+{
+	char arr[];
+
+	printf("simple calculator with the following operations; '-', '+', '*', '/'\n");
+	printf("to use type num space operation space number. (e.g 3 * 5) then enter\n");
+	scanf("%s", &arr);
+	for(i = 0; arr[i] != NULL; i++)
+	{
+		if(arr[i] !=<F11><F11>
