cb8bfe469504294c4778f5dfcc65088025ff2682
Mayor termux <adurasheyiakinlolu@gmail.com>
2025-04-03 21:40:55 +0100

    structs challenge 1!!
    Changes to be committed:
    	new file:   01-cat.h
    	new file:   1-kuter.c
    Just got out of a slump, gonna need to go at plash speed!

---
+++ alexis/0_01-c_prog/13-structs/01-cat.h
@@ -0,0 +1,18 @@
+#ifndef CAT_H
+#define CAT_H
+
+/**
+ * cat - a structure that contains basic cat data
+ * @name: name of the cat
+ * @owner: name of the owner
+ * @age: age of the cat
+ **/
+
+struct cat
+{
+	char *name;
+	char *owner;
+	double age;
+};
+
+#endif
---
+++ alexis/0_01-c_prog/13-structs/1-kuter.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "01-cat.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    struct cat my_cat;
+
+    my_cat.name = "luna";
+    my_cat.age = 5.4;
+    my_cat.owner = "Mayor";
+    printf("My name is %s, and I am %.1f :) - Meow\n", my_cat.name, my_cat.age);
+    return (0);
+}
